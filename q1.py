c6c90f5f939d1d38b41331d9b4b16a4475afb0f7
Fardee001 <70164338+Fardee001@users.noreply.github.com>
2020-08-29 14:09:09 +0530

    assignement 1

---
+++ q1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("Myself Fathima Fardee \n student from lbt \n tc 29/1089 dRA 1009 ,\n thirumala  \n Trivandrum \nPhne num: 7895892359")
---
+++ q33.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 20:07:24 2020
+
+@author: Acer
+"""
+#3
+name="fathima fardee"
+print("your name is",name)
